749661ab7ec5ec66e0fb9368c355c6a8ed98cb47
digaogalo <digaolimac@gmail.com>
2023-03-19 17:22:51 -0300

    add color in tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,23 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
+  <h1 class="text-3xl font-bold underline bg-red-300">
     Hello world!
   </h1>
+  <MyComponent />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MyComponent from './components/MyComponent.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MyComponent
+  },
+})
+</script>
+
 
 
